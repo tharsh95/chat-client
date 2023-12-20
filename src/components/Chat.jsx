bf1bc8235c32d3778134636ef7388ef41ebfbb93
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2023-12-21 01:48:32 +0530

    initial com mit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const YourChatComponent = ({ socket, room, name }) => {
   const [message, setMessage] = useState("");
   const [rData, setRData] = useState([]);
+  const navigate = useNavigate();
+
   const send = async () => {
     if (room && name) {
       await socket.emit("send", {
@@ -11,7 +14,7 @@ const YourChatComponent = ({ socket, room, name }) => {
         name,
         // time:new Date(Date.now()).getHours
       });
-      //   setMessage("");
+      setMessage("");
     }
   };
 
@@ -19,46 +22,54 @@ const YourChatComponent = ({ socket, room, name }) => {
     socket.on("receive", (data) => {
       setRData((prevData) => [...prevData, data]);
     });
-  }, [socket]);
+    if(room===""){
+      console.log("try again")
+      navigate('/')
+    }
+  }, [socket,room]);
   return (
-    <div className="bg-slate-800 overflow-scroll w-1/2 mx-auto shadow-md rounded-md p-4 flex flex-col h-screen">
-      {rData?.map((el) => (
-        <div
-          className={`m-2 p-4 rounded-lg  ${
-            el.name === name
-              ? "bg-white  flex flex-col justify-end"
-              : "bg-green-200"
-          }`}
-        >
-          <p className="text-lg">{el.message}</p>
-          <p className="text-xs">
-            {" "}
-            <span className="text-xs">By:</span>
-            {name === el.name ? "You" : el.name}
-          </p>
+    <>
+      <div className="bg-slate-800 overflow-scroll w-1/2 mx-auto shadow-md rounded-md p-4 flex flex-col h-screen">
+        <h1 className="text-white">User:{name ? name : "Not connected"}</h1>
+        <h2 className="text-white">Room:{room ? room : "Not connected"}</h2>
+        {rData?.map((el) => (
+          <div
+            className={`m-2 p-4 rounded-lg  ${
+              el.name === name
+                ? "bg-white  flex flex-col justify-end"
+                : "bg-green-200"
+            }`}
+          >
+            <p className="text-lg">{el.message}</p>
+            <p className="text-xs">
+              {" "}
+              <span className="text-xs">By:</span>
+              {name === el.name ? "You" : el.name}
+            </p>
+          </div>
+        ))}
+        <div className="mt-auto p-4    flex justify-center  sticky bottom-0 z-10">
+          <input
+            type="text"
+            placeholder="Type your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="p-2 border border-gray-300  rounded-md mr-2 w-[100vw]"
+          />
+          <button
+            onClick={send}
+            disabled={message === "" || room === ""}
+            className={`p-2 rounded-md ${
+              message === ""
+                ? "bg-slate-200 cursor-not-allowed text-black"
+                : "bg-blue-600 text-white"
+            }`}
+          >
+            Send
+          </button>
         </div>
-      ))}
-      <div className="mt-auto p-4    flex justify-center  sticky bottom-0 z-10">
-        <input
-          type="text"
-          placeholder="Type your message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="p-2 border border-gray-300  rounded-md mr-2 w-[100vw]"
-        />
-        <button
-          onClick={send}
-          disabled={message === ""}
-          className={`p-2 rounded-md ${
-            message === ""
-              ? "bg-slate-200 cursor-not-allowed text-black"
-              : "bg-blue-600 text-white"
-          }`}
-        >
-          Send
-        </button>
       </div>
-    </div>
+    </>
   );
 };
 
