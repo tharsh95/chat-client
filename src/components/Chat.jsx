31227021cabbc3bba777fe40e4cb7bc97ec50506
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2023-12-21 22:23:31 +0530

    added leave room

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,9 @@
-
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-const YourChatComponent = ({setName,setRoom, socket, room, name }) => {
+const YourChatComponent = ({ setRoom, setName, socket, room, name }) => {
   const [message, setMessage] = useState("");
-  const [rData, setRData] = useState([]);
+  const [_, setRData] = useState([]);
   const navigate = useNavigate();
 
   const send = async () => {
@@ -13,12 +12,14 @@ const YourChatComponent = ({setName,setRoom, socket, room, name }) => {
         room,
         message,
         name,
+        time: `${new Date(Date.now()).getHours()}:${new Date(
+          Date.now()
+        ).getMinutes()}`,
       });
       setMessage("");
       scrollToBottom();
     }
   };
-
   const chatContainerRef = useRef(null);
 
   // Function to scroll to the last element
@@ -26,78 +27,72 @@ const YourChatComponent = ({setName,setRoom, socket, room, name }) => {
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop =
         chatContainerRef.current.scrollHeight;
+      // chatContainerRef.current.scrollTop += 20;
       setTimeout(() => {
-        chatContainerRef.current.scrollTop += 200;
+        chatContainerRef.current.scrollTop += 20;
       }, 100);
     }
   };
-
   useEffect(() => {
     const msgs = [];
     socket.on("receive", (data) => {
       msgs.push(data);
       sessionStorage.setItem("chats", JSON.stringify(msgs));
       setRData((prevData) => [...prevData, data]);
-      scrollToBottom();
-      // Add a temporary class to the new message for styling
-      const newMessageIndex = msgs.length - 1;
-      const chatMessages = document.querySelectorAll(".chat-message");
-      chatMessages[newMessageIndex].classList.add("new-message");
-      // Remove the temporary class after a delay (e.g., 3000 milliseconds)
-      setTimeout(() => {
-        chatMessages[newMessageIndex].classList.remove("new-message");
-      }, 3000);
     });
     if (room === "") {
       navigate("/");
     }
   }, [socket, room]);
-
   return (
     <>
       <div
-        className="bg-slate-800 overflow-scroll w-1/2 mx-auto shadow-md rounded-md p-4 flex flex-col h-screen"
+        className="bg-slate-800 w-1/2  mx-auto shadow-md rounded-md p-4 flex flex-col h-screen"
         ref={chatContainerRef}
       >
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mb-4">
           <h1 className="text-white">User:{name ? name : "Not connected"}</h1>
           <h2 className="text-white">Room:{room ? room : "Not connected"}</h2>
           <button
             className="bg-cyan-300 p-4 rounded-lg"
             onClick={() => {
               sessionStorage.clear();
-              navigate('/')
-              socket.disconnect();
-              setName("")
-              setRoom("")
+              navigate("/");
+              setRoom("");
+              setName("");
             }}
           >
             Leave Room
           </button>
         </div>
-        {JSON.parse(sessionStorage.getItem("chats"))?.map((el, index) => (
-          <div
-            key={index}
-            className={`m-2 p-4 rounded-lg chat-message ${
-              el.name === name
-                ? "bg-white flex flex-col justify-end"
-                : "bg-green-200"
-            } ${index === rData.length - 1 ? "new-message" : ""}`}
-          >
-            <p className="text-lg">{el.message}</p>
-            <p className="text-xs">
-              <span className="text-xs">By:</span>
-              {name === el.name ? "You" : el.name}
-            </p>
-          </div>
-        ))}
-        <div className="mt-auto p-4 flex justify-center sticky bottom-0 z-10">
+        <div className="overflow-scroll">
+          {JSON.parse(sessionStorage.getItem("chats"))?.map((el) => (
+            <div
+              className={`m-2 p-4 rounded-lg  ${
+                el.name === name ? "bg-white " : "bg-green-200"
+              }`}
+            >
+              <div>
+                <p className="text-lg">{el.message}</p>
+                <p className="text-xs">
+                  {" "}
+                  <span className="text-xs">By:</span>
+                  {name === el.name ? "You" : el.name}
+                </p>
+              </div>
+              <div>
+                <p className="text-xs">{el.time}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="mt-auto p-4    flex justify-center  sticky bottom-0 z-10">
           <input
             type="text"
             placeholder="Type your message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className="p-2 border border-gray-300 rounded-md mr-2 w-[100vw]"
+            className="p-2 border border-gray-300  rounded-md mr-2 w-[100vw]"
           />
           <button
             onClick={send}
